9a0696c5c8b3fa15ce9e169b11018061e457eb98
Johanson Chew <alexenderjohanson@yahoo.com>
2016-12-27 03:41:15 +0800

    fixing json stringify

--- src/controllers/new_thread.js
+++ src/controllers/new_thread.js
@@ -10,9 +10,9 @@ module.exports.handle = function (sender, params) {
   // 2.My Favorite Bots
   // 3.Delete All
 
-  let browseButton = fb.generateButton('postback', 'Browse Categories', {action: 'browse'})
-  let favoriteButton = fb.generateButton('postback', 'My Favorite Bots', {action: 'my_favorite'})
-  let deleteAllButton = fb.generateButton('postback', 'Delete All', {action: 'delete_all'})
+  let browseButton = fb.generateButton('postback', 'Browse Categories', JSON.stringify({action: 'browse'}))
+  let favoriteButton = fb.generateButton('postback', 'My Favorite Bots', JSON.stringify({action: 'my_favorite'}))
+  let deleteAllButton = fb.generateButton('postback', 'Delete All', JSON.stringify({action: 'delete_all'}))
 
   let buttons = [browseButton, favoriteButton, deleteAllButton]
   fb.sendButtonMessage(sender, 'What type of robot are you looking for?', buttons)

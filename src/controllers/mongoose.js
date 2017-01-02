3504f737dd4d3024ede49715468762187a2cd97c
Locus <yu.locus@gmail.com>
2017-01-02 14:30:50 +0800

    update mongoose

--- src/controllers/mongoose.js
+++ src/controllers/mongoose.js
@@ -1,8 +1,8 @@
-const config = require("config");
+const config = require('config')
 
 const MONGODB_URI = (process.env.MONGODB_URI) ?
   (process.env.MONGODB_URI) :
-  config.get('mongodbURI');
+  config.get('mongodbURI')
 
 var mongoose = require("mongoose");
 mongoose.connect(MONGODB_URI);

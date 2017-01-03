65cf22dedbd4c6394ad4be2c588acbf0f69f0c8a
Johanson Chew <alexenderjohanson@yahoo.com>
2017-01-03 15:14:15 +0800

    adding example

--- src/controllers/browse_categories.js
+++ src/controllers/browse_categories.js
@@ -1,9 +1,12 @@
 'use strict'
 
 const fb = require('./facebook')
+const botList = require('./bot_list')
 
 module.exports.handle = function (sender, params) {
-    //TODO: implement browse
+  // TODO: implement browse
 
-    fb.sendTextMessage(sender, 'TODO: Implement Browse')
+  botList.listCategories().then(function (categories) {
+    fb.sendTextMessage(sender, JSON.stringify(categories))
+  })
 }
